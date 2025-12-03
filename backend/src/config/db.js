6aac1c28c64d4a2002867d76a77e02ea35f0bf28
Bernadette Chrestella <dc.bernadette@praisindo.com>
2025-12-03 10:47:57 +0700

    feat: Update .gitignore and package configuration for environment handling and add cross-env dependency

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,26 @@
 const mysql = require("mysql2");
+const path = require("path");
+
+require("dotenv").config({
+  path:
+    process.env.NODE_ENV === "production"
+      ? path.resolve(__dirname, "../../.env.production")
+      : path.resolve(__dirname, "../../.env.local"),
+});
+
+const isProduction = process.env.NODE_ENV === "production";
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: isProduction
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
 });
 
 db.connect((err) => {
